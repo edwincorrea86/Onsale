6d2b16e07f9522b9caaa90f79fc1b08d7ed93cca
EDWIN CORREA ARIAS <edwincorrea28236@correo.itm.edu.co>
2021-07-10 21:41:28 -0500

    crud countries

---
+++ OnSale.Common/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OnSale.Common.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20, ErrorMessage ="El campo {0} debe de tener menos de {1}")]
+        [Required]
+        public string Name { get; set; }
+
+
+    }
+}
